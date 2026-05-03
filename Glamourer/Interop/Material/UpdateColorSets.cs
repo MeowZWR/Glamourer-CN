f13b378f88677e1940d09d2dc070813bcebd19f9
MeowZ#WR <antabakala@hotmail.com>
2026-05-03 09:42:58 +0800

    Failed match against expected documentation. -CN

--- Glamourer/Interop/Material/UpdateColorSets.cs
+++ Glamourer/Interop/Material/UpdateColorSets.cs
@@ -1,23 +1,25 @@
-﻿using Luna;
+﻿using FFXIVClientStructs.FFXIV.Client.Graphics.Scene;
+using Luna;
 using Penumbra.GameData;
 using Penumbra.GameData.Interop;
 
 namespace Glamourer.Interop.Material;
 
-public sealed class CreateNewModel : FastHook<CreateNewModel.Delegate>
+public sealed unsafe class CreateNewModel : FastHook<CreateNewModel.Delegate>
 {
-    public delegate void Delegate(Model model, uint unk);
+    public delegate nint Delegate(CharacterBase* characterBase, uint slot);
 
     private readonly ThreadLocal<Model> _updatingModel = new(() => Model.Null);
 
     public CreateNewModel(HookManager hooks)
         => Task = hooks.CreateHook<Delegate>("Create New Model", Sigs.CreateNewModel, Detour, true);
 
-    private void Detour(Model model, uint modelSlot)
+    private nint Detour(CharacterBase* characterBase, uint modelSlot)
     {
-        _updatingModel.Value = model;
-        Task.Result!.Original(model, modelSlot);
+        _updatingModel.Value = characterBase;
+        var ret = Task.Result!.Original(characterBase, modelSlot);
         _updatingModel.Value = Model.Null;
+        return ret;
     }
 
     public Model Get()
